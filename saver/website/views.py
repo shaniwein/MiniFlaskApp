e128ddd1c06f2a89842d5516965958480301704e
wshani <weinshani@gmail.com>
2017-01-17 17:47:38 +0200

    Added some views.

--- saver/website/views.py
+++ saver/website/views.py
@@ -5,23 +5,27 @@ from .models        import Client, Command, Result
 from .serializers   import ClientSerializer, CommandSerializer, ResultSerializer
 
 class IndexView(generic.ListView):
-
-    template_name       = 'website/clients.html'
+    template_name       = 'website/index.html'
     context_object_name = 'latest_clients_list'
 
     def get_queryset(self):
-        return Client.objectes.filter(
+        return Client.objects.filter(
             created__lte=timezone.now()
         ).order_by('-created')[:10]
+
+class DetailView(generic.DetailView):
+    model = Client 
+    template_name = 'website/detail.html'
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all().order_by('-created')
     serializer_class = ClientSerializer 
 
 class CommandViewSet(viewsets.ModelViewSet):
-    queryset = Command.objects.all().order_by('-created')
+    queryset = Command.objects.all().order_by('created')
     serializer_class = CommandSerializer
 
 class ResultViewSet(viewsets.ModelViewSet):
-    queryset = Result.objects.all().order_by('-created')
+    queryset = Result.objects.all().order_by('created')
     serializer_class = ResultSerializer 
+
